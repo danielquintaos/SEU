7fd3bce5fe982ca186a32070ce5b4deb4cb6049d
danielquintaos <daniel.davila@sou.inteli.edu.br>
2022-05-05 18:53:33 -0300

    melhorias no currículo

--- 03_AUT_EST_ENTREGA/Semana 2/app_11.js
+++ 03_AUT_EST_ENTREGA/Semana 2/app_11.js
@@ -1,10 +1,8 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3011;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Curriculo incompleto de Daniel Quintao Davila</title></head> \
+ <!DOCTYPE html> 
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"></link> 
+  <title>Curriculo incompleto de Daniel Quintao Davila</title></head> \
               <body> \
                 <div id="main"> \
                        <h1>Daniel Quintao Davila - Curriculo (incompleto)</h1> \
@@ -12,9 +10,6 @@ const server = http.createServer((req, res) => {
                        <H2>Experiencia de trabalho: padronizacao de planilhas Excel na Davila Arquitetura;</H2> </div> \
                        <H2>Outras informacoes uteis: fluente em ingles e portugues e sabe espanhol basico; possui capacidade significativamente acima da media de redigir textos;</H2> </div> \
                        <H2>Outras informacoes: possui resistencia fisica e mental de um superatleta; possui entendimento cultural elevado; possui facilidade de interagir socialmente com outros individuos e com grupos de individuos <blink>(no sentido Deleuziano do termo)</blink>.</H2> </div> \
-              </body> \
+                       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+               </body> \
            </html>');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
