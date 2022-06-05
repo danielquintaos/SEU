1d569628cd857b0f42191952ad477633444a029c
danielquintaos <daniel.davila@sou.inteli.edu.br>
2022-06-05 18:39:39 -0300

    curriculo semana 6 concluido

--- 03_AUT_EST_ENTREGA/Semana 6/backend/app_61.js
+++ 03_AUT_EST_ENTREGA/Semana 6/backend/app_61.js
@@ -1,9 +1,9 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3061;
-const sqlite3 = require('sqlite3').verbose(); 
+const sqlite3 = require('sqlite3').verbose();
 const DBPATH = 'dbUser.db';
 
 const bodyParser = require('body-parser');
@@ -18,69 +18,69 @@ app.use(express.json());
 /****** CRUD ******************************************************************/
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/users', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+app.get('/education', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  var db = new sqlite3.Database(DBPATH); // Abre o banco
-var sql = 'SELECT * FROM tbUser ORDER BY title COLLATE NOCASE';
-  db.all(sql, [],  (err, rows ) => {
-      if (err) {
-          throw err;
-      }
-      res.json(rows);
-  });
-  db.close(); // Fecha o banco
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM education';
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
 });
 
 // Insere um registro (é o C do CRUD - Create)
 app.post('/userinsert', urlencodedParser, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
-  var db = new sqlite3.Database(DBPATH); // Abre o banco
-  db.run(sql, [],  err => {
-      if (err) {
-          throw err;
-      }
-  });
-  db.close(); // Fecha o banco
-  res.end();
+	sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
 });
 
 // Atualiza um registro (é o U do CRUD - Update)
 app.post('/userupdate', urlencodedParser, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  sql = "UPDATE tbUser SET title = '" + req.body.title + "' WHERE userId = " + req.body.userId;
-  var db = new sqlite3.Database(DBPATH); // Abre o banco
-  db.run(sql, [],  err => {
-      if (err) {
-          throw err;
-      }
-      res.end();
-  });
-  db.close(); // Fecha o banco
+	sql = "UPDATE tbUser SET title = '" + req.body.title + "' WHERE userId = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
 });
 
 // Exclui um registro (é o D do CRUD - Delete)
 app.post('/userdelete', urlencodedParser, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-  sql = "DELETE FROM tbUser WHERE userId = " + req.body.userId;
-  var db = new sqlite3.Database(DBPATH); // Abre o banco
-  db.run(sql, [],  err => {
-      if (err) {
-          throw err;
-      }
-      res.end();
-  });
-  db.close(); // Fecha o banco
+	sql = "DELETE FROM tbUser WHERE userId = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
