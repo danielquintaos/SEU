a443f81bb249cc7c62e468626776ff3494faea64
danielquintaos <daniel.davila@sou.inteli.edu.br>
2022-06-10 12:06:06 -0300

    erro durante tentativa de ex. pers. etapa 7

---
+++ 03_AUT_EST_ENTREGA/Semana 8/frontend/script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    document.getElementById("demo").style.fontSize = "25px";
+  document.getElementById("demo").style.color = "red";
+  document.getElementById("demo").style.backgroundColor = "black";        
+}
+
+//$(document).ready(function() {
+  $("#hidden").hover(function() {
+    $(this).css("color", "black");
+}),
+function() {
+(this).hide();
+};
+
+var getResDiv = "#get";
+
+// /* Função que faz uma requisição GET */
+function TestGET(){
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", url, false);
+xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+
+$(getResDiv).append("<br />" + xhttp.responseText);
+$(getResDiv).append("<br />Seleção do <i>title</i>: " + JSON.parse(xhttp.response).title);
+console.log(xhttp.responseText);
+}
+
+function teste(){
+  $('#mymodal').modal('show');
+}
